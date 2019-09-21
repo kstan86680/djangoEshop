46fcba87a10c2502af4a3acfec884879d7a7c12d
kstan86680 <kstan86680@gmail.com>
2019-09-21 19:13:08 +0600

    s3 storage added

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -8,6 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(
 
 SECRET_KEY = config('SECRET_KEY')
 
+AWS_STORAGE_BUCKET_NAME = 'django-eshop-bucket'
+AWS_S3_REGION_NAME = 'ap-southeast-1'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAIKPSPBYM7AP2JMXQ'
+AWS_SECRET_ACCESS_KEY = 'RFuQ2C1MKT/qks0W8MLltEgTw6jYFK/IXl99yUba'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -23,7 +30,9 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_countries',
 
-    'core'
+    'core',
+
+    'storage'
 ]
 
 MIDDLEWARE = [
@@ -65,13 +74,16 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = '/static/'
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Auth
 
