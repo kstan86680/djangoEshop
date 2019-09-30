001c2037493cf2628cfd862df951843d043bc8a6
Calvin <kstan86680@gmail.com>
2019-09-30 23:05:10 +0800

    t1

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Product, Cart, CartItem
 # Register your models here.
-
+#  Test
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'featured', 'recommended')
 
